f9a03a775cc6046ea40b734827c3ed8a4f8e9be8
Prince Ali Khan <princealikhan08@gmail.com>
2018-08-29 13:14:32 +0530

    include global doc style

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Sidebar from '../components/sidebar'
-import './index.css'
+// import './index.css'
+import './global-style.css'
 
 const Layout = ({ children, data }) => (
   <div>
