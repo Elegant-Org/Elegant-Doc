901ef585d33fefe74ab20dee36f025671f63e484
Prince Ali Khan <princealikhan08@gmail.com>
2018-09-03 11:44:11 +0530

    Markdown Config

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
  // You can delete this file if you're not using it
+
+const path = require('path');
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+  const docTemplate = path.resolve(`src/templates/docs-template.js`);
+  return graphql(`{
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___title] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            html
+            id
+            frontmatter {
+              path
+              pageTitle
+              pageShortTitle
+              description
+            }
+          }
+        }
+      }
+    }`)
+    .then(result => {
+      if (result.errors) {
+        return Promise.reject(result.errors);
+      }
+      result.data.allMarkdownRemark.edges
+        .forEach(({ node }) => {
+          createPage({
+            path: node.frontmatter.path,
+            component: docTemplate,
+            context: {}
+          });
+        });
+    });
+}
