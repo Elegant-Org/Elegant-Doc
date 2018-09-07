ac13b4dfea68fdfb069973ba37625fb041238ec2
Prince Ali Khan <princealikhan08@gmail.com>
2018-09-07 11:37:37 +0530

    color page added

---
+++ src/styles/shade.js
@@ -0,0 +1,69 @@
+export const StyleShade = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  rgb: {
+    fontSize: '12px'
+  },
+  primary: {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      paddingRight: '1.5em',
+      paddingBottom: '1.5em',
+    },
+    colorBox: {
+        height: '180px',
+        width: '180px',
+        borderRadius: '20px',
+        marginBottom:'5px'
+    },
+    colors: {
+      purple: {
+        backgroundColor: '#79589E'
+      },
+      blue: {
+        backgroundColor: '#3268B9'
+      },
+      black: {
+        backgroundColor: '#4E535A'
+      }
+    }
+  },
+  secondary: {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      paddingRight: '1.5em',
+      paddingBottom: '1.5em',
+    },
+    colorBox: {
+        height: '120px',
+        width: '120px',
+        borderRadius: '20px',
+        marginBottom:'5px'
+    },
+    colors: {
+      silver: {
+        backgroundColor: '#E3E7EF'
+      },
+      red: {
+        backgroundColor: '#FD7272'
+      },
+      yellow: {
+        backgroundColor: '#FFB944'
+      },
+      white: {
+        backgroundColor: '#FFFFFF'
+      },
+      green: {
+        backgroundColor: '#1ABC9C'
+      },
+      gray: {
+        backgroundColor: '#96A3B5'
+      }
+    }
+  },
+};
