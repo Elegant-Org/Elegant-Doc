2884a8dbe4e5bab4a94cedf66abba918f12fc0f5
Prince Ali Khan <princealikhan08@gmail.com>
2018-10-07 01:47:07 +0530

    component sidebar and style sidebar added

---
+++ src/components/component-sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ComponentSidebar extends Component {
+
+  render() {
+    return (
+      <div className="navigation">
+        Compoenent Sidebar
+      </div>
+    );
+  }
+}
---
+++ src/components/style-sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class StyleSidebar extends Component {
+
+  render() {
+    return (
+      <div className="navigation">
+        Style Sidebar
+      </div>
+    );
+  }
+}
