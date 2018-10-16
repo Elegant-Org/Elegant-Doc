24000cff667b8102c89d9441d85507654808d74a
Prince Ali Khan <princealikhan08@gmail.com>
2018-10-16 10:17:21 +0530

    sidebar nav implemented

---
+++ src/helpers/global-constants.js
@@ -0,0 +1,20 @@
+import StyleModifier from '../assets/img/style-modifier.svg'
+import StyleTypography from '../assets/img/style-modifier.svg'
+
+export const styleMenu = [
+    {
+        "name": "Typography",
+        "icon": StyleTypography,
+        "link": "typography"
+    },
+    {
+        "name": "Modifiers",
+        "icon": StyleModifier,
+        "link": "modifier"
+    },
+    {
+        "name": "Dimensions",
+        "icon": "http://via.placeholder.com/50x50",
+        "link": "dimension"
+    }
+];
